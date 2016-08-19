712650536e7d1056318360b88c37a2705585b00c
Kim Nobay <kim@nobay.co.uk>
2016-08-19 12:59:45 +0100

    Added comments

--- shuffler.js
+++ shuffler.js
@@ -1,19 +1,21 @@
 var cardsontable = 50;
-var hand = []; //wierdly, this didn't work when I tried to initalize as type numeber
-var deck = [];
-var debug = ["Program started"];
-var table = [false];
-var i = 0;
-var cardsatatime = 0;
-var cardspickedthistime = 0;
-var cardtoDeal = 0;
+var i = 0; // generic counter
+var cardsatatime = 0; // will use to store the value in the dropdown
+var cardspickedthistime = 0; // needed in a loop later on
+var cardtoDeal = 0; // a variable used later
+var debug = ["Program started"]; // an array for logging debugging messages in
+var hand = []; // this represents the cards that have been dealt out of the deck
+var deck = []; // these are the cards that have been picked off the table but haven't been dealt yet
+var table = [true]; // this represents the table, not the values at each position, just whether there is a card there or not
 for (i = 1; i <= cardsontable; i++) {
     table[i] = true;
 }
+// Make a card invisible, and hence not clickable, on the table
 function hideCard(id) {
     var el = document.getElementById(id);
     el.style.visibility = "hidden";
 }
+// Try to pick a card off the table at the requested position if there are still cards on the table
 function pickCard(numberSent) {
     if (cardsontable > 0) {
         hideCard(numberSent);
@@ -22,9 +24,10 @@ function pickCard(numberSent) {
         table[numberSent] = false; //remember that card is no longer on the table
     }
 }
+// Pick cards off the table as many times as required by the dropdown
 function autoPick() {
     cardspickedthistime = 0;
-    var cardsatatime = parseInt(document.getElementById("deal-number").value);
+    var cardsatatime = parseInt(document.getElementById("deal-number").value); //get the value from the drop down as an integer
     while (cardspickedthistime < cardsatatime && cardsontable > 0) {
         var myRandomNumber = Math.floor(Math.random() * 50);
         if (table[myRandomNumber + 1] == true) {
@@ -34,7 +37,7 @@ function autoPick() {
     }
 }
 function dealCard(dummyindex) {
-    var numbertodeal = parseInt(document.getElementById("deal-number").value);
+    var numbertodeal = parseInt(document.getElementById("deal-number").value); //get the value from the dropdown as an integer
     for (i = 0; i < numbertodeal; i++) {
         var myRandomNumber = Math.floor(Math.random() * deck.length);
         cardtoDeal = parseInt(deck.splice(myRandomNumber, 1).toString());
@@ -43,7 +46,7 @@ function dealCard(dummyindex) {
             hand.push(cardtoDeal);
         }
         else {
-            debug.push("No card found");
+            debug.push("No card found"); // can't imagine why this would ever happen, probably redundant'
         }
     }
 }
--- shuffler.ts
+++ shuffler.ts
@@ -1,22 +1,26 @@
 var cardsontable = 50;
-var hand = []; //wierdly, this didn't work when I tried to initalize as type numeber
-var deck = [];
-var debug = ["Program started"];
-var table = [false];
-var i = 0;
-var cardsatatime = 0;
-var cardspickedthistime = 0;
-var cardtoDeal = 0;
+var i = 0; // generic counter
+var cardsatatime = 0;  // will use to store the value in the dropdown
+var cardspickedthistime = 0; // needed in a loop later on
+var cardtoDeal = 0; // a variable used later
 
+var debug = ["Program started"]; // an array for logging debugging messages in
+var hand = []; // this represents the cards that have been dealt out of the deck
+var deck = []; // these are the cards that have been picked off the table but haven't been dealt yet
+var table = [true]; // this represents the table, not the values at each position, just whether there is a card there or not
 for (i = 1; i <= cardsontable; i++) {
     table[i] = true;
 }
 
+
+// Make a card invisible, and hence not clickable, on the table
 function hideCard(id) {
     var el = document.getElementById(id);
     el.style.visibility = "hidden";
 }
 
+
+// Try to pick a card off the table at the requested position if there are still cards on the table
 function pickCard(numberSent) {
     if (cardsontable > 0) {
         hideCard(numberSent);
@@ -26,22 +30,23 @@ function pickCard(numberSent) {
     }
 }
 
+
+// Pick cards off the table as many times as required by the dropdown
 function autoPick() {
     cardspickedthistime = 0;
-    var cardsatatime = parseInt((<HTMLInputElement>document.getElementById("deal-number")).value);
+    var cardsatatime = parseInt((<HTMLInputElement>document.getElementById("deal-number")).value); //get the value from the drop down as an integer
 
-    while (cardspickedthistime < cardsatatime && cardsontable > 0) {
+    while (cardspickedthistime < cardsatatime && cardsontable > 0) {  // keep trying to pick up cards until you've reached the number in the dropdown or there are no cards left on the table
         var myRandomNumber = Math.floor(Math.random() * 50);
-        if (table[myRandomNumber + 1] == true) {
+        if (table[myRandomNumber + 1] == true) {  // only pick up a card if it's available on the table
             pickCard(myRandomNumber + 1);
             cardspickedthistime++;
         }
     }
 }
 
-function dealCard(dummyindex) {
-    var numbertodeal = parseInt((<HTMLInputElement>document.getElementById("deal-number")).value);
-
+function dealCard(dummyindex) { // deal cards from the deck to the hand
+    var numbertodeal = parseInt((<HTMLInputElement>document.getElementById("deal-number")).value); //get the value from the dropdown as an integer
     for (i = 0; i < numbertodeal; i++) {
         var myRandomNumber = Math.floor(Math.random() * deck.length);
         cardtoDeal = parseInt(deck.splice(myRandomNumber, 1).toString());
@@ -50,12 +55,12 @@ function dealCard(dummyindex) {
             hand.push(cardtoDeal);
         }
         else {
-            debug.push("No card found");
+            debug.push("No card found");  // can't imagine why this would ever happen, probably redundant'
         }
     }
 }
 
-function sortHand() {
+function sortHand() {  // sort the hand in memory, clear what's visibily in the hand then re render the hand.
     hand.sort(function (a, b) { return a - b });
     document.getElementById("myPanel").innerHTML = "";
     for (i = 0; i < hand.length; i++) {
@@ -63,7 +68,7 @@ function sortHand() {
     }
 }
 
-function outputDebug() {
+function outputDebug() {  // put some standard stuff in the debug log then output it with whatever else has been logged there.
     debug.push("Deck now contains [" + deck + "]");
     debug.push("Hand now contains [" + hand + "]");
     while (debug.length > 0) {
